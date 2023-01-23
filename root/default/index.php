cf61f1c0286f4ac60a4f5b6974894c949c1f50a5
Kei Sawamura <sawamura-k3@itec.hankyu-hanshin.co.jp>
2023-01-23 16:30:52 +0900

    Add index.php

---
+++ root/default/index.php
@@ -0,0 +1,4 @@
+<?php
+
+    $url = "http://localhost:8081/index.html";
+    var_dump(file_get_contents($url));
--- root/hotels/index.php
+++ root/hotels/index.php
@@ -1,6 +1,4 @@
 <?php
 
-    echo "Hello, World!!";
-
     $url = "http://localhost:8081/index.php";
     var_dump(file_get_contents($url));
